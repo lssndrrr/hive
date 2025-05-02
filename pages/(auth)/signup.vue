9e0e67ef62949cb1cb52d112cc10a834310e1072
Lanieza <lblopez@up.edu.ph>
2025-05-02 20:46:21 +0800

    [feat][lanie] updated sign up page and main.css

--- pages/(auth)/signup.vue
+++ pages/(auth)/signup.vue
@@ -2,7 +2,7 @@
     <div class="flex min-h-screen items-center justify-center">
         <AppHeader />
         <div
-            class="w-full max-w-sm p-10 bg-white rounded-2xl shadow-lg space-y-15 text-black"
+            class="w-full max-w-sm p-10 bg-white rounded-2xl shadow-lg space-y-10 text-black"
         >
             <h1
                 class="text-5xl text-primary"
@@ -10,8 +10,68 @@
             >
                 Signup
             </h1>
-            <form @submit.prevent="signup" class="flex flex-col space-y-6">
-                <div class="flex flex-col space-y-2">
+            <form @submit.prevent="signup" class="flex flex-col space-y-2">
+                <div class="flex flex-col space-y-1">
+                    <label
+                        for="firstName"
+                        class="text-neutral"
+                        style="font-family: 'Nexa'; font-weight: 200"
+                    >
+                        First Name
+                    </label>
+                    <UInput
+                        id="firstName"
+                        v-model="firstName"
+                        type="text"
+                        variant="outline"
+                        inputClass="bg-white text-black placeholder:text-gray-500"
+                        :style="{ color: 'black' }"
+                        :ui="{
+                            base: 'bg-white',
+                        }"
+                    />
+                </div>
+                <div class="flex flex-col space-y-1">
+                    <label
+                        for="lastName"
+                        class="text-neutral"
+                        style="font-family: 'Nexa'; font-weight: 200"
+                    >
+                        Last Name
+                    </label>
+                    <UInput
+                        id="lastName"
+                        v-model="lastName"
+                        type="text"
+                        variant="outline"
+                        inputClass="bg-white text-black placeholder:text-gray-500"
+                        :style="{ color: 'black' }"
+                        :ui="{
+                            base: 'bg-white',
+                        }"
+                    />
+                </div>
+                <div class="flex flex-col space-y-1">
+                    <label
+                        for="username"
+                        class="text-neutral"
+                        style="font-family: 'Nexa'; font-weight: 200"
+                    >
+                        Username
+                    </label>
+                    <UInput
+                        id="username"
+                        v-model="username"
+                        type="text"
+                        variant="outline"
+                        inputClass="bg-white text-black placeholder:text-gray-500"
+                        :style="{ color: 'black' }"
+                        :ui="{
+                            base: 'bg-white',
+                        }"
+                    />
+                </div>
+                <div class="flex flex-col space-y-1">
                     <label
                         for="email"
                         class="text-neutral"
@@ -31,7 +91,7 @@
                         }"
                     />
                 </div>
-                <div class="flex flex-col space-y-2">
+                <div class="flex flex-col space-y-1">
                     <label
                         for="password"
                         class="text-neutral"
@@ -68,7 +128,7 @@
                         </template>
                     </UInput>
                 </div>
-                <div class="flex flex-col space-y-2">
+                <div class="flex flex-col space-y-1">
                     <label
                         for="confirm-password"
                         class="text-neutral"
@@ -139,6 +199,9 @@ const email = ref('')
 const password = ref('')
 const confirmPassword = ref('')
 const show = ref(false)
+const username = ref('')
+const firstName = ref('')
+const lastName = ref('')
 
 const signup = async () => {
     if (password.value !== confirmPassword.value) {
