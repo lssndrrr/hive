32d82c7f28165373f6837fbc83b7afa44cc300a5
Lanieza <lblopez@up.edu.ph>
2025-05-02 20:35:38 +0800

    [feat][lanie] changed email to username field

--- pages/(auth)/login.vue
+++ pages/(auth)/login.vue
@@ -13,16 +13,16 @@
             <form @submit.prevent="login" class="flex flex-col space-y-6">
                 <div class="flex flex-col space-y-2">
                     <label
-                        for="email"
+                        for="username"
                         class="text-neutral"
                         style="font-family: 'Nexa'; font-weight: 200"
                     >
-                        Email
+                        Username
                     </label>
                     <UInput
-                        id="email"
-                        v-model="email"
-                        type="email"
+                        id="username"
+                        v-model="username"
+                        type="text"
                         variant="outline"
                         inputClass="bg-white text-black placeholder:text-gray-500"
                         :style="{ color: 'black' }"
@@ -98,11 +98,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const email = ref('')
+const username = ref('')
 const show = ref(false)
 const password = ref('')
 
 const login = async () => {
-    console.log('Logging in with:', email.value, password.value)
+    console.log('Logging in with:', username.value, password.value)
 }
 </script>
