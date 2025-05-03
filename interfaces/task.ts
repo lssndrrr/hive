9258a5559eb4dd73cd9b353c24b60c2edfe8ec4f
Lanieza <lblopez@up.edu.ph>
2025-05-04 00:11:53 +0800

    [feat][lanie] added priority and description in task interface

--- interfaces/task.ts
+++ interfaces/task.ts
@@ -1,10 +1,13 @@
 export type Status = 'TD' | 'IP' | 'D' | 'OD'
+export type Priority = 'H' | 'M' | 'L'
 
 export interface Task {
     id: number
     name: string
     assignee: number | null
+    description: string
     status: Status
+    priority: Priority
     due_date: string
     created_on: string
     hive: number
